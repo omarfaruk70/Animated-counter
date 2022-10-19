1984a434fd80fc7e9460c87094bf90b6fe9abdd7
Abdullah Al Nomaan <83067643+SaikatNoman12@users.noreply.github.com>
2022-10-19 08:19:17 +0600

    Update script.js
    You must clearInterval().The setInterval() variable must be passed as a clearInterval() parameter. Because this variable serves as a reference to setInterval().

--- script.js
+++ script.js
@@ -2,13 +2,19 @@
  * for projects
  */
 let value = 0;
+
 const counter1 = setInterval(projectCounter, 10);
+
 function projectCounter(){
-    // value++;
+    
+    value++;
+    
+    if(value === 250){
+        clearInterval(counter1);
+    }
+    
     document.getElementById('projects').innerHTML = value;
-    if(value == 250){
-        clearInterval(projectCounter);
-    }      
+    
 };
 /**
  * for clients
