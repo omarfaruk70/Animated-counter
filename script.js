c7cdb5bef531d23d76f4f3b5f3eb51bc5865c6f6
farukfci70 <faruk1611281892@gmail.com>
2022-10-20 01:14:54 +0600

    added all section

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
 /**
  * for projects
  */
-let value = 0;
-const counter1 = setInterval(projectCounter, 10);
+let projectValue = 0;
+const counter1 = setInterval(projectCounter, 20);
 function projectCounter(){
-    // value++;
-    document.getElementById('projects').innerHTML = value;
-    if(value == 250){
-        clearInterval(projectCounter);
-    }      
+    projectValue++;
+    if(projectValue === 250){
+        clearInterval(counter1);
+    }    
+    document.getElementById('projects').innerHTML = projectValue;  
 };
+
+
 /**
  * for clients
  */
+let clientValue = 0 ;
+const counter2 = setInterval(clientCounter, 10);
+function clientCounter(){
+    clientValue++;
+    if(clientValue === 500 ){
+        clearInterval(counter2)
+    }
+    document.getElementById('clients').innerHTML = clientValue;
+};
 
 
 /**
  * for achievement
  */
+let achievedValue = 0;
+let counter3 = setInterval(achievedCounter, 20);
+function achievedCounter() {
+    achievedValue++;
+    if(achievedValue === 300){
+        clearInterval(counter3)
+    }
+    document.getElementById('achieved').innerHTML = achievedValue;
+};
